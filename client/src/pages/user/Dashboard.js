f708a8b9b347248f9662feb360447871cebffa88
Alba Romero Kauss <albaromerokauss@Albas-MBP.yallo.box>
2023-06-04 20:58:08 +0200

    feat: dashboard styling

--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -1,4 +1,6 @@
 import { useAuth } from "../../context/auth";
+import { NavLink } from "react-router-dom";
+import styling from "./Dashboard.module.css";
 import UserMenu from "../../components/nav/UserMenu";
 
 export default function UserDashboard() {
@@ -9,18 +11,27 @@ export default function UserDashboard() {
   return (
     <>
       {/* <pre>{JSON.stringify(auth, null, 4)}</pre> */}
-      <div className="container-fluid">
+      <div className={`container-fluid ${styling.containerMain}`}>
         <div className="row">
           <div className="col-md-3">
             <UserMenu />
           </div>
-
-          <div className="col-md-9">
-            <div className="p-3 mt-2 mb-2 h4 bg-light">User Info</div>
-            <ul className="list-group">
+          <div className={`${styling.dashboardDiv} col-md-9`}>
+            <h2>👋 Ciao {auth.user?.firstName.toUpperCase()} </h2>
+            <h2>Il tuo account</h2>
+            <p>Dal tuo account Gioachino puoi gestire i tuoi ordini, resi e dati di contatto.</p>
+            <div className={styling.centerDiv}>
+              <p>Trovi qui le info sui tuoi aggiornamenti, resi o rimborsi.</p>
+              <button className={styling.buttonShop}>
+                <NavLink className={styling.buttonShopText} to="/">
+                  Continua lo shopping
+                </NavLink>
+              </button>
+            </div>
+            {/* <ul className="list-group">
               <li className="list-group-item">{auth.user?.firstName}</li>
               <li className="list-group-item">{auth.user?.email}</li>
-            </ul>
+            </ul> */}
           </div>
         </div>
       </div>
