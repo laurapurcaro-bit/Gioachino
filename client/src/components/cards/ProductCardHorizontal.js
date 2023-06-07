31d1a0861f558b948a5d0d6cb04468288a66f9e8
laurapurcaro-bit <laura.p@doit-intl.com>
2023-06-07 15:36:22 +0200

    added translation for shop

--- client/src/components/cards/ProductCardHorizontal.js
+++ client/src/components/cards/ProductCardHorizontal.js
@@ -1,4 +1,5 @@
 import moment from "moment";
+import { Trans } from "react-i18next";
 
 export default function ProductCartHorizontal({ p, removeFromCart }) {
   const currency = "EUR";
@@ -8,14 +9,15 @@ export default function ProductCartHorizontal({ p, removeFromCart }) {
     <div className="row g-0">
       <div className="col-md-4">
         <img
-          src={`${process.env.REACT_APP_API}/product/photo/${p._id}`}
+          src={`${
+            process.env.REACT_APP_S3_HTTP_BUCKET_DEV
+          }/products/${p.categorySlug.toLowerCase()}/${p._id}-main.png`}
           alt={p.name}
           style={{
             height: "100%",
             width: "100%",
             // fix this!!!!!!!
             // objectFit: "cover",
-
             marginLeft: "-10%",
             borderBottomRightRadius: "0px",
           }}
@@ -23,11 +25,19 @@ export default function ProductCartHorizontal({ p, removeFromCart }) {
       </div>
       <div className="col-md-4">
         <div className="card-body">
-          <h5 className="card-title"> {p.name} </h5>
+          <h5 className="card-title">
+            <Trans>{p.name}</Trans>
+          </h5>
           <p className="card-text">
-            {p.description.length < 50
-              ? `${p.description}`
-              : `${p.description.substring(0, 50)}...`}
+            {p.description.length < 50 ? (
+              <p>
+                <Trans>{p.description}</Trans>
+              </p>
+            ) : (
+              <p>
+                <Trans>{p.description.substring(0, 50)}...</Trans>
+              </p>
+            )}
           </p>
           <p className="card-text">
             {p?.price?.toLocaleString(localString, {
@@ -36,21 +46,21 @@ export default function ProductCartHorizontal({ p, removeFromCart }) {
             })}
           </p>
           {/* <p className="card-text">Quantity: {p.count}</p> */}
-          <p className="card-text">Quantity: {p.quantity}</p>
+          <p className="card-text"><Trans>Quantity</Trans>: {p.quantity}</p>
         </div>
       </div>
 
       <div className="d-flex justify-content-between">
         <p className="card-text">
-          <small className="text-muted">
+          {/* <small className="text-muted">
             Listed {moment(p.createdAt).fromNow()}
-          </small>
+          </small> */}
         </p>
         <p
           className="text-danger mb-2 pointer"
           onClick={() => removeFromCart(p._id)}
         >
-          Remove
+          <Trans>Remove</Trans>
         </p>
       </div>
     </div>
