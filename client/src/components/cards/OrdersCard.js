c74fa64267885e55c75b4173ee67f5b35afd0cec
laurapurcaro-bit <laura.p@doit-intl.com>
2023-06-29 10:13:22 +0200

    order status

--- client/src/components/cards/OrdersCard.js
+++ client/src/components/cards/OrdersCard.js
@@ -1,50 +1,44 @@
 import { Trans } from "react-i18next";
+import styling from "./OrdersCard.module.css"
 
 export default function OrdersCard({ p }) {
   const currency = "EUR";
   const localString = "en-US";
   console.log("ORDERS CARD", p);
   return (
-    <div className="card mb-3">
+    <div className="mb-3">
       <div className="row g-0">
         <div className="col-md-4">
           <img
             src={`${process.env.REACT_APP_S3_HTTP_BUCKET_DEV}/products/${p.category.slug}/${p._id}-0.png`}
             alt={p.name}
             style={{
-              height: "100%",
-              width: "90%",
-              // fix this!!!!!!!
-              // objectFit: "cover",
+              height: "80px",
+              width: "80px",
               marginLeft: "0%",
               borderBottomRightRadius: "0px",
             }}
           />
         </div>
         <div className="col-md-4">
-          <div className="card-body">
-            <h5 className="card-title">
+          <div className={styling.productText}>
+            <h5 className="">
               {" "}
               <Trans>{p.name}</Trans>{" "}
             </h5>
-            <p className="card-text">
-              {p.description?.length < 50 ? (
-                <p>
-                  <Trans>{p?.description}</Trans>
-                </p>
-              ) : (
-                <p>
-                  <Trans>{p?.description?.substring(0, 50)}...</Trans>
-                </p>
-              )}
-            </p>
-            <p className="card-text">
+            <p className="">
               {p?.price?.toLocaleString(localString, {
                 style: "currency",
                 currency: currency,
               })}
             </p>
-            <p className="card-text">
+            <p className="">
+              <Trans>color</Trans>: {p.color}
+            </p>
+            <p className="">
+              <Trans>size</Trans>: {p.size}
+            </p>
+            <p className="">
               <Trans>Quantity</Trans>: {p.quantity}
             </p>
           </div>
--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -3,15 +3,22 @@ import UserMenu from "../../components/nav/UserMenu";
 import OrdersCard from "../../components/cards/OrdersCard";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import moment from "moment";
+import styling from "./Orders.module.css";
 import { Trans } from "react-i18next";
+import { DownOutlined, UpOutlined } from "@ant-design/icons";
 
 export default function UserOrders() {
   // context
   const [auth] = useAuth();
   // state
   const [orders, setOrders] = useState([]);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   localStorage.removeItem("order");
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
   useEffect(() => {
     if (auth?.token) {
@@ -33,6 +40,14 @@ export default function UserOrders() {
     }
   };
 
+  const options = {
+    weekday: "short",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    timeZone: "UTC",
+  };
+
   return (
     <>
       {/* <pre>{JSON.stringify(auth, null, 4)}</pre> */}
@@ -43,52 +58,193 @@ export default function UserOrders() {
           </div>
 
           <div className="col-md-9">
-            <div className="p-3 mt-2 mb-2 h4 bg-light"><Trans>Orders</Trans></div>
-
-            {orders?.map((order, index) => {
-              return (
-                <div
-                  className="border shadow bg-light rounded-4 mb-3"
-                  key={order._id}
-                >
-                  <table className="table table-bordered">
-                    <thead>
-                      <tr>
-                        <th scope="col">#{order._id}</th>
-                        <th scope="col"><Trans>Status</Trans></th>
-                        <th scope="col"><Trans>Ordered</Trans></th>
-                        <th scope="col"><Trans>Payment</Trans></th>
-                        <th scope="col"><Trans>Quantity</Trans></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{index + 1}</td>
-                        <td><Trans>{order?.orderStatus}</Trans></td>
-                        <td>{order?.buyer?.firstName}</td>
-                        <td>{moment(order?.createdAt).fromNow()}</td>
-                        <td>
-                          {order?.paymentInfo.success ? <p><Trans>Success</Trans></p> : <p><Trans>Failed</Trans></p>}
-                        </td>
-                        <td>{order?.products?.length}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <div className="container">
-                    <div className="row m-3">
-                      {order?.cart.map((p, i) => (
-                        <div key={p._id} className="mb-2">
-                          <OrdersCard p={p} />
+            <div className="p-3 mt-2 mb-2 h4 bg-light">
+              <Trans>Orders</Trans>
+            </div>
+            <div className={`row`}>
+              {orders?.map((order, index) => {
+                return (
+                  <>
+                    <div className={`col-md-12 ${styling.ordersContainer}`}>
+                      {/* Top bar */}
+                      <div className={`bg-light ${styling.topBar}`}>
+                        <p>
+                          <Trans>Order #</Trans>
+                          {order.orderId}
+                        </p>
+                        <div className={styling.colElements}>
+                          <p>
+                            <Trans>Order total</Trans>:
+                          </p>
+                          <p>
+                            <strong>{order.amount}€</strong>
+                          </p>
+                          <div className={styling.dropdownContainer}>
+                            <div onClick={toggleDropdown}>
+                              <p className={styling.manageOrder}>
+                                <Trans>Manage order</Trans>
+                                {dropdownOpen ? (
+                                  <>
+                                    {" "}
+                                    <UpOutlined />
+                                  </>
+                                ) : (
+                                  <>
+                                    {" "}
+                                    <DownOutlined />
+                                  </>
+                                )}
+                              </p>
+                              {dropdownOpen && (
+                                <div className={styling.dropdownMenu}>
+                                  <ul>
+                                    <li><p>Return</p></li>
+                                    <li><p>Rate</p></li>
+                                  </ul>
+                                </div>
+                              )}
+                            </div>
+                          </div>
                         </div>
-                      ))}
+                      </div>
+                      <div className={`row ${styling.ordersInfo}`}>
+                        {/* Product info */}
+                        <div className="col-md-3">
+                          <h3>
+                            <Trans>Items</Trans>
+                          </h3>
+                          {order?.cart.map((p, i) => (
+                            <div key={p._id} className="mb-2">
+                              <OrdersCard p={p} />
+                            </div>
+                          ))}
+                        </div>
+                        {/* Date */}
+                        <div className="col-md-3">
+                          <h3>
+                            <Trans>Date</Trans>
+                          </h3>
+                          <p>
+                            {order?.createdAt
+                              ? new Date(order.createdAt)
+                                  .toLocaleDateString("en-GB", options)
+                                  .replace(/\//g, "-")
+                              : ""}
+                          </p>
+                          <h3>
+                            <Trans>Payment status</Trans>
+                          </h3>
+                          <p>
+                            {order?.paymentInfo.success ? (
+                              <p>
+                                <Trans>Paid</Trans>
+                              </p>
+                            ) : (
+                              <p>
+                                <Trans>Not paid</Trans>
+                              </p>
+                            )}
+                          </p>
+                        </div>
+                        {/* Shipping info */}
+                        <div className="col-md-3">
+                          <h3>
+                            <Trans>Shipping info</Trans>
+                          </h3>
+                          <div className="">
+                            <p>
+                              {order.shippingAddress.name}{" "}
+                              {order.shippingAddress.surname}
+                            </p>
+                            <p>{order.shippingAddress.street}</p>
+                            <p>
+                              {order.shippingAddress.city},{" "}
+                              {order.shippingAddress.zip},{" "}
+                              {order.shippingAddress.country}
+                            </p>
+                          </div>
+                        </div>
+                        {/* Shipping status */}
+                        <div className="col-md-3">
+                          <h3>
+                            <Trans>Package Status</Trans>
+                          </h3>
+                          <p>
+                            <Trans>{order?.orderStatus}</Trans>
+                          </p>
+                          <button className={styling.trackButton}>
+                            <Trans>Track your package</Trans>
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              );
-            })}
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
     </>
   );
 }
+
+// {orders?.map((order, index) => {
+//   return (
+//     <div
+//       className="border shadow bg-light rounded-4 mb-3"
+//       key={order._id}
+//     >
+//       <table className="table table-bordered">
+//         <thead>
+//           <tr>
+//             <th scope="col">#{order._id}</th>
+//             <th scope="col">
+//               <Trans>Status</Trans>
+//             </th>
+//             <th scope="col">
+//               <Trans>Ordered</Trans>
+//             </th>
+//             <th scope="col">
+//               <Trans>Payment</Trans>
+//             </th>
+//             <th scope="col">
+//               <Trans>Quantity</Trans>
+//             </th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>{index + 1}</td>
+//             <td>
+//               <Trans>{order?.orderStatus}</Trans>
+//             </td>
+//             <td>{order?.buyer?.firstName}</td>
+//             <td>{moment(order?.createdAt).fromNow()}</td>
+//             <td>
+//               {order?.paymentInfo.success ? (
+//                 <p>
+//                   <Trans>Success</Trans>
+//                 </p>
+//               ) : (
+//                 <p>
+//                   <Trans>Failed</Trans>
+//                 </p>
+//               )}
+//             </td>
+//             <td>{order?.products?.length}</td>
+//           </tr>
+//         </tbody>
+//       </table>
+//       <div className="container">
+//         <div className="row m-3">
+//           {order?.cart.map((p, i) => (
+//             <div key={p._id} className="mb-2">
+//               <OrdersCard p={p} />
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// })}
